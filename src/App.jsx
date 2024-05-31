b2321a73d776380e5439004f26f10c0909bc4775
JacquelineHJChen <118964879+JacquelineHJChen@users.noreply.github.com>
2024-05-31 13:42:06 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+import StopWatch from './StopWatch.jsx'
+
+function App() {
+
+
+  return (
+    <div>
+      <StopWatch />
+    </div>
+  )
+}
+
+export default App
